f5c92f7e77b2de36d33fde7c8f392fc9311965a4
jesus <jesusantonio30122016@gmail.com>
2021-10-02 11:42:22 -0500

    Fix bash script not detecting prescence of stivale2.h and stivale.h correctly

--- compile.sh
+++ compile.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-
 export PATH="$PATH:$HOME/opt/cross/s390-linux/bin"
-if ! test -f stage2/stivale.h; then
+
+if [ ! -f stage2/stivale.h ]; then
     wget https://github.com/stivale/stivale/raw/master/stivale.h
     mv stivale.h stage2/stivale.h
 fi
-if ! test -f stage2/stivale2.h; then
+if [ ! -f stage2/stivale2.h ]; then
     wget https://github.com/stivale/stivale/raw/master/stivale2.h
     mv stivale2.h stage2/stivale2.h
 fi
 
+make clean || exit
 make -j || exit
+
+gcc -Wall -Wextra tools/bin2rec.c -o tools/bin2rec -lm
 
 s390-linux-objcopy -O binary stage1/stage1 stage1/stage1.bin || exit
 ./tools/bin2rec stage1/stage1.bin stage1.txt || exit
